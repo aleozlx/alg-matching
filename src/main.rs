70c19a60742ae55275493b135634786f2f6d3718
Alex Yang <aleozlx@gmail.com>
2018-08-17 01:39:21 -0500

    - Clone

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,57 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
 
-trait Graph<V, E>
-  where V: Clone
-{
-    fn bfs_shortest_path(&self, s: V, t: V) -> Vec<V>;
+trait Graph<V, E> {
+    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Vec<&'g V>;
     fn count_vertices(&self) -> usize;
-    // fn adjacent_vertices<'a>(&'a self, u: V) -> Box<Iterator<Item=V> + 'a>;
 }
 
 struct AdjGraph<V, E>
-  where V: std::hash::Hash + std::cmp::Eq + Clone
+  where V: std::hash::Hash + std::cmp::Eq
 {
     adj: HashMap<V, HashMap<V, E>>
 }
 
 impl<V, E> Graph<V, E> for AdjGraph<V, E>
-  where V: std::hash::Hash + std::cmp::Eq + Clone
+  where V: std::hash::Hash + std::cmp::Eq
 {
-    fn bfs_shortest_path(&self, s: V, t: V) -> Vec<V> {
+    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Vec<&'g V> {
         let mut q = VecDeque::new();
-        let mut pred: HashMap<V, V> = HashMap::new();
-        q.push_back(s.clone());
-        while !q.is_empty() {
-            let u = q.pop_front().unwrap();
-            for v in self.adj.keys() {
-                if !pred.contains_key(&v) && *v!=s {
-                    pred.insert(v.clone(), u.clone());
-                    q.push_back(v.clone());
+        let mut pred: HashMap<&V, &V> = HashMap::new();
+        q.push_back(s);
+        while let Some(u) = q.pop_front() {
+            if let Some(adj) = self.adj.get(u) {
+                for v in adj.keys() {
+                    if !pred.contains_key(v) && v!=s {
+                        pred.insert(v, u);
+                        q.push_back(v);
+                    }
                 }
             }
         }
-        let mut inv = Vec::new();
-        let mut p = &t;
-        while pred.contains_key(&p) {
-            inv.push(pred[p].clone());
-            p = &pred[p];
+        let mut inv = vec![t];
+        let mut p = t;
+        while let Some(&pre) = pred.get(p) {
+            inv.push(pre);
+            p = pre;
         }
-        Vec::new()
+        inv.reverse();
+        return inv;
     }
 
     fn count_vertices(&self) -> usize {
         self.adj.len()
     }
-
-    // fn adjacent_vertices<'a>(&'a self, u: V) -> Box<Iterator<Item=V> + 'a> {
-    //     if self.adj.contains_key(&u) {
-    //         let k = self.adj[&u].keys();
-    //         Box::new(k)
-    //     }
-    //     else {
-    //         Box::new(std::iter::empty())
-    //     }
-    // }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut g: AdjGraph<i32, ()> = AdjGraph { adj: HashMap::new() };
+    g.adj.insert(0, [3, 2].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
+    g.adj.insert(2, [1].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
+    g.adj.insert(3, [4].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
+    g.adj.insert(4, [1].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
+    let p = g.bfs_shortest_path(&0, &1);
+    for i in p {
+        println!("{}", i);
+    }
 }
