81c25acc17847d9bd822c77060ad1c343ad3e47a
Alex Yang <aleozlx@gmail.com>
2018-08-17 03:14:14 -0500

    can't believe this runs...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::iter::FromIterator;
 
 trait Graph<V, E> {
-    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Vec<&'g V>;
+    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Option<Vec<&'g V>>;
     fn count_vertices(&self) -> usize;
 }
 
@@ -15,9 +16,10 @@ struct AdjGraph<V, E>
 impl<V, E> Graph<V, E> for AdjGraph<V, E>
   where V: std::hash::Hash + std::cmp::Eq
 {
-    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Vec<&'g V> {
+    fn bfs_shortest_path<'g>(&'g self, s: &'g V, t: &'g V) -> Option<Vec<&'g V>> {
+        if s == t { return Some(vec![s]); }
         let mut q = VecDeque::new();
-        let mut pred: HashMap<&V, &V> = HashMap::new();
+        let mut pred = HashMap::new();
         q.push_back(s);
         while let Some(u) = q.pop_front() {
             if let Some(adj) = self.adj.get(u) {
@@ -29,14 +31,13 @@ impl<V, E> Graph<V, E> for AdjGraph<V, E>
                 }
             }
         }
-        let mut inv = vec![t];
-        let mut p = t;
-        while let Some(&pre) = pred.get(p) {
-            inv.push(pre);
-            p = pre;
+        let mut ret = std::collections::LinkedList::new();
+        ret.push_front(*pred.get(t)?);
+        while let Some(pre) = pred.get(ret.front().unwrap()) {
+            ret.push_front(pre);
         }
-        inv.reverse();
-        return inv;
+        ret.push_back(t);
+        Some(Vec::from_iter(ret))
     }
 
     fn count_vertices(&self) -> usize {
@@ -44,14 +45,28 @@ impl<V, E> Graph<V, E> for AdjGraph<V, E>
     }
 }
 
+macro_rules! unweighted_graph(
+    { $($u:expr => [ $($v:expr),+ ]),+ } => {
+        {
+            let mut g = AdjGraph { adj: HashMap::new() };
+            $(
+                g.adj.insert($u, [$($v),+].iter().cloned().map(|v| (v, 1)).collect());
+            )+
+            g
+        }
+     };
+);
+
 fn main() {
-    let mut g: AdjGraph<i32, ()> = AdjGraph { adj: HashMap::new() };
-    g.adj.insert(0, [3, 2].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
-    g.adj.insert(2, [1].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
-    g.adj.insert(3, [4].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
-    g.adj.insert(4, [1].iter().cloned().map(|v| (v, ())).collect::<HashMap<_, _>>());
-    let p = g.bfs_shortest_path(&0, &1);
-    for i in p {
-        println!("{}", i);
+    let g = unweighted_graph! {
+        0 => [3, 2],
+        2 => [1],
+        3 => [4],
+        4 => [1]
+    };
+    if let Some(path) = g.bfs_shortest_path(&0, &1){
+        for v in path {
+            println!("{}", v);
+        }
     }
 }
