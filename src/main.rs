ccfb97ac6dc84bceed38136349cb06a6be5f6249
Alex Yang <aleozlx@gmail.com>
2018-08-16 23:15:42 -0500

    + Graph definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
+trait Graph<V, E> {
+    fn bfs_shortest_path(s: V, t: V) -> Vec<V> {
+        Vec::new()
+    }
+}
+
+struct AdjGraph<V, E> where V: std::hash::Hash {
+    adj: HashMap<V, HashMap<V, E>>
+}
+
 fn main() {
     println!("Hello, world!");
 }
